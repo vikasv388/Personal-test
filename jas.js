b572206b8b81b6c0a3d55c3bc3a7136020177c15
vikasv388 <vikasvkybrix@gmail.com>
2026-09-10 17:41:08 +0530

    Add jas.js with personal information logging

---
+++ jas.js
@@ -0,0 +1,12 @@
+var name = "David";
+let age = 20;
+const city = "Delhi";
+
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("City:", city);
+
+// let can be changed
+age = 21;
+
+console.log("New Age:", age);
