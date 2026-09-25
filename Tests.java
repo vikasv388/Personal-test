2e850b2b0274b3f82dc21796ad6f9e8d42a0ed2d
vikasv388 <vikasvkybrix@gmail.com>
2026-09-25 16:45:43 +0530

    Add Selenium demo for Google search automation

---
+++ Tests.java
@@ -0,0 +1,22 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class SeleniumDemo {
+    public static void main(String[] args) {
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.google.com");
+
+        driver.findElement(By.name("q"))
+              .sendKeys("Selenium Java");
+
+        driver.findElement(By.name("q"))
+              .submit();
+
+        System.out.println(driver.getTitle());
+
+        driver.quit();
+    }
+}
